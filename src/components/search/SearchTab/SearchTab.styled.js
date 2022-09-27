00ea626df7eb5d025a62c7d218cde23b64297736
Namkyung <srirachacho@gmail.com>
2022-09-27 22:24:37 +0900

    Feat: 검색창 컴포넌트의 UI를 구현합니다.

---
+++ src/components/search/SearchTab/SearchTab.styled.js
@@ -0,0 +1,57 @@
+import styled from '@emotion/styled';
+
+const SearchTabContainer = styled.div`
+  width: 100%;
+  padding: 100px 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #cfebfc;
+`;
+
+const SearchTabHeader = styled.h1`
+  text-align: center;
+  font-weight: bold;
+  font-size: 2rem;
+  line-height: 1.6;
+  margin: 0 0 20px 0;
+`;
+
+const SearchTabForm = styled.form`
+  width: 50%;
+  padding: 10px 10px 10px 15px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  max-width: 320px;
+  margin: 0 auto;
+  background-color: #ffffff;
+  border: 0;
+  border-radius: 42px;
+`;
+
+const SearchTabInput = styled.input`
+  width: 100%;
+  height: 40px;
+  border: none;
+  font-size: 1rem;
+  :focus {
+    outline: none;
+  }
+`;
+
+const SearchTabButton = styled.button`
+  width: 35px;
+  height: 35px;
+  border-radius: 42px;
+  border: 0;
+  background-color: #3e8df4;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+`;
+
+export { SearchTabContainer, SearchTabHeader, SearchTabForm, SearchTabInput, SearchTabButton };
