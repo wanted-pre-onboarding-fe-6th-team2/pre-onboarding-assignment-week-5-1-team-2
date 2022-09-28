743b8c868a9043fb625686735f7de26eb31d3f94
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-09-28 14:19:29 +0900

    Feat: 불필요한 api 로직 삭제

--- src/api/sick.js
+++ src/api/sick.js
@@ -1,15 +1,5 @@
 import api from '@/api';
 
-export const getSicks = async () => {
-  try {
-    const response = await api.get(`/sick`);
-
-    return response.data;
-  } catch (error) {
-    throw new Error(error);
-  }
-};
-
 export const getSick = async keyword => {
   try {
     const response = await api.get(`/sick?q=${keyword}`);
