30a956abe119017ae166084e62c2bf9536d34c9d
jyoon <wldbszp@gmail.com>
2022-09-28 15:17:11 +0900

    Chore: Home 페이지 원상 복구

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,24 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import searchApiService from '@/api/search';
+import React from 'react';
 
-const Home = () => {
-  const [keyword, setKeyword] = useState('');
-  const [results, setResults] = useState([]);
-  useEffect(() => {
-    const searchResults = async () => {
-      const response = await searchApiService.getSearch({ keyword });
-      setResults(response);
-    };
-    searchResults();
-  }, [keyword]);
-  return (
-    <div>
-      <input onChange={e => setKeyword(e.target.value)} />
-      {results.map(result => (
-        <div key={result.sickCd}>{result.sickNm}</div>
-      ))}
-    </div>
-  );
-};
+const Home = () => <div>Home</div>;
 
 export default Home;
