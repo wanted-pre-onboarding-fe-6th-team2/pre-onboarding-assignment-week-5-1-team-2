b270d5deff9fc38d25b9882914cc80a7713dc647
Taak-e <kjtag0408@gmail.com>
2022-09-29 09:58:36 +0900

    Refactor: api 호출에 문제로 우선 동작을 위한 수정

--- src/api/searchApiService.js
+++ src/api/searchApiService.js
@@ -2,7 +2,7 @@ import http from '@/api';
 
 export const searchBySickName = async ({ keyword }) => {
   try {
-    const response = await http.get(`/sick?sickNm=${keyword}`);
+    const response = await http.get(`/sick?q=${keyword}`);
 
     return response.data;
   } catch (error) {
